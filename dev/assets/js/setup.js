616a946cff34995766ff2b95dcdb25dbb9570488
Matteo Crippa <m.crippa@matteocrippa.it>
2015-01-11 14:51:14 +0100

    cleaned file and structure

---
+++ dev/assets/js/setup.js
@@ -0,0 +1,45 @@
+if (localStorage.getItem('database')) {
+  location.href = 'database.html';
+}
+
+$('#pwdfile').on('click', function() {
+  var $this = $(this)
+  $this.change(function(evt) {
+    //console.log($(this).val())
+  })
+})
+
+$('#password').on('blur', function() {
+  var $this = $(this);
+  if ($this.val().trim().length < 1) {
+    $this.prev().removeClass('hide');
+    return false;
+  } else {
+    $this.prev().addClass('hide');
+    return true;
+  }
+});
+
+function triggerFormVal() {
+  if ($('#pwdfile').val().length > 0 && $('#password').val().trim().length > 0) {
+    return true;
+  } else {
+    $('#pwdfile,#password').trigger('blur');
+    return false;
+  }
+}
+
+$('#login').on('click', function() {
+  if (triggerFormVal()) {
+    $('#form-val').addClass('hide');
+
+    localStorage.setItem('database', $('#pwdfile').val())
+
+    dash.setupDB($('#pwdfile').val(), $('#password').val())
+
+    location.href = 'database.html'
+
+  } else {
+    $('#form-val').removeClass('hide');
+  }
+});
