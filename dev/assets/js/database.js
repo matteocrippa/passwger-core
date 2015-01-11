6c4b0db419712929e776153c8bc83893cb6e8b50
Matteo Crippa <matteo.crippa@gmail.com>
2015-01-12 00:08:38 +0100

    cleaned unuseful js functions

--- dev/assets/js/database.js
+++ dev/assets/js/database.js
@@ -74,7 +74,51 @@
     }
     
     dbc.filterEntry = function() {
-      
+      //populateList(getCurrentFolder(), $('#filter-entry').val())
+    }
+    
+    dbc.populateList = function() {
+      /*filter = populateList.arguments.length < 2 ? null : filter;
+
+  $('#fold-' + folder).addClass('active')
+
+  $('#password-list').children().children().remove()
+
+  if (db(folder)) {
+
+    if (db(folder).value().length > 0) {
+
+      $.each(db(folder).sortBy('name').filter(function(item) {
+        if (filter) {
+          return item.name.toLowerCase().indexOf(filter.toLowerCase()) > -1
+        } else {
+          return true
+        }
+      }).value(), function(index, value) {
+
+        var classItem = ""
+
+        if (index % 2 == 0) {
+          classItem = "class='unread'"
+        }
+
+        var item = "<tr " + classItem + "> \
+        <td class='small-col'><div class='domain-icon' style='background-image: url('assets/img/placeholder-icon.png');'></div></td> \
+        <td class='name'><a href='#'>" + value.name +
+          "</a></td> \
+        </tr>"
+
+        $('#password-list').append(item)
+
+      })
+    } else {
+      $('#password-list').append('<tr><td class="name">No items stored yet.</td></tr>')
+    }
+
+
+  } else {
+    $('#password-list').append('<tr><td class="name">No items stored yet.</td></tr>')
+  }*/
     }
 
   }])
@@ -88,41 +132,11 @@ var db = null
 
 $(document).ready(function() {
 
-  if (!db) {
-    document.title = 'Unlock Database // Passwger'
-    $('#unlock-box').removeClass('hide')
-  }
-
-  $('#filter-entry').on('keyup', function() {
-    populateList(getCurrentFolder(), $('#filter-entry').val())
-  })
-
   /*$('#a-password').on('click', function() {
     deselectAllFolder()
     populateList('password')
     prepareAddEntry('password')
     $('#btn-addnew').html('<i class="fa fa-pencil"></i> Add new password')
-  })
-
-  $('#a-wifi').on('click', function() {
-    deselectAllFolder()
-    populateList('wifi')
-    prepareAddEntry('wifi')
-    $('#btn-addnew').html('<i class="fa fa-pencil"></i> Add new wifi')
-  })
-
-  $('#a-ccard').on('click', function() {
-    deselectAllFolder()
-    populateList('ccard')
-    prepareAddEntry('ccard')
-    $('#btn-addnew').html('<i class="fa fa-pencil"></i> Add new credit card')
-  })
-
-  $('#a-server').on('click', function() {
-    deselectAllFolder()
-    prepareAddEntry('server')
-    populateList('server')
-    $('#btn-addnew').html('<i class="fa fa-hdd-o"></i> Add new server')
   })*/
 
   $('#saveEntry').on('click', function(e) {
@@ -196,93 +210,6 @@ $(document).ready(function() {
 
 })
 
-function populateList(folder, filter) {
-
-  filter = populateList.arguments.length < 2 ? null : filter;
-
-  $('#fold-' + folder).addClass('active')
-
-  $('#password-list').children().children().remove()
-
-  if (db(folder)) {
-
-    if (db(folder).value().length > 0) {
-
-      $.each(db(folder).sortBy('name').filter(function(item) {
-        if (filter) {
-          return item.name.toLowerCase().indexOf(filter.toLowerCase()) > -1
-        } else {
-          return true
-        }
-      }).value(), function(index, value) {
-
-        var classItem = ""
-
-        if (index % 2 == 0) {
-          classItem = "class='unread'"
-        }
-
-        var item = "<tr " + classItem + "> \
-        <td class='small-col'><div class='domain-icon' style='background-image: url('assets/img/placeholder-icon.png');'></div></td> \
-        <td class='name'><a href='#'>" + value.name +
-          "</a></td> \
-        </tr>"
-
-        $('#password-list').append(item)
-
-      })
-    } else {
-      $('#password-list').append('<tr><td class="name">No items stored yet.</td></tr>')
-    }
-
-
-  } else {
-    $('#password-list').append('<tr><td class="name">No items stored yet.</td></tr>')
-  }
-
-}
-
-function showTotItemsFolder() {
-
-  if (db.object.password) {
-    $('#fold-password a').html('<i class="fa fa-key"></i> Password (' + db.object.password.length + ')')
-  } else {
-    $('#fold-password a').html('<i class="fa fa-key"></i> Password (0)')
-  }
-
-  if (db.object.wifi) {
-    $('#fold-wifi a').html('<i class="fa fa-rss"></i> Wi-Fi (' + db.object.wifi.length + ')')
-  } else {
-    $('#fold-wifi a').html('<i class="fa fa-rss"></i> Wi-Fi (0)')
-  }
-
-  if (db.object.server) {
-    $('#fold-server a').html('<i class="fa fa-hdd-o"></i> Server (' + db.object.server.length + ')')
-  } else {
-    $('#fold-server a').html('<i class="fa fa-hdd-o"></i> Server (0)')
-  }
-
-  /*if(db.object.identity){
-  $('#fold-identity a').html('<i class="fa fa-male"></i> Identity ('+db.object.identity.length+')')
-}else{
-$('#fold-identity a').html('<i class="fa fa-male"></i> Identity (0)')
-}*/
-
-  if (db.object.ccard) {
-    $('#fold-ccard a').html('<i class="fa fa-credit-card"></i> Credit Card (' + db.object.ccard.length + ')')
-  } else {
-    $('#fold-ccard a').html('<i class="fa fa-credit-card"></i> Credit Card (0)')
-  }
-
-}
-
-function deselectAllFolder() {
-
-  $('#folder-menu li').each(function(index, value) {
-    $(value).removeClass('active')
-  })
-
-}
 
 function prepareAddEntry(folder) {
 
@@ -303,18 +230,4 @@ function prepareAddEntry(folder) {
   }
 
   $('#form-' + folder).removeClass('hide')
-}
-
-function getCurrentFolder() {
-
-  var folderId = 'password'
-
-  $('#folder-menu li').each(function(index, value) {
-
-    if ($(value).hasClass('active')) {
-      folderId = $(value).attr('id').toString().replace('fold-', '')
-    }
-  })
-
-  return folderId
 }
