f41254b9732957103b4e3dea140586a404920010
Matteo Crippa <m.crippa@matteocrippa.it>
2015-01-15 18:45:16 +0100

    updated in order to use nwjs

--- builder.js
+++ builder.js
@@ -1,10 +1,12 @@
 var NwBuilder = require('node-webkit-builder');
 var nw = new NwBuilder({
   files: './dev/**', // use the glob format
+  downloadUrl: 'http://dl.nwjs.io/',
   //platforms: ['osx32', 'osx64', 'win32', 'win64']
   platforms: ['osx64'],
   macIcns: './icon/icon.icns',
-  winIco: './icon/icon.ico'
+  winIco: './icon/icon.ico',
+  version: '0.11.5'
 });
 
 //Log stuff you want
