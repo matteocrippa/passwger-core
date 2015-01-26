d975ecc1b3f8a618fc5391adb16caa0e9e19c559
Matteo Crippa <m.crippa@matteocrippa.it>
2015-01-26 20:58:52 +0100

    updated setup page

--- dev/controller/setup.js
+++ dev/controller/setup.js
@@ -1,17 +1,46 @@
-window.dash = window.dash || {}
+(function() {
 
-var low = require('lowdb')
+  if (localStorage.getItem('database')) {
+    location.href = '../routers/database.html';
+  }
 
-dash.setupDB = function(path, pwd) {
+  angular
+    .module('setup', ['file-model', 'ngMaterial', 'ngMessages'])
 
-  var db = low(path, {
-    encrypt: true,
-    passkey: pwd
-  })
+  function SetupController($log, $window) {
+    var vm = this
 
-  db('settings').push({
-    created: new Date().getTime()
-  })
+    vm.error = ""
 
-  return true
-}
+    vm.createDb = function() {
+      if (!vm.pwdfile) {
+        vm.error = "You must select where to store your db!"
+      } else if (!vm.password) {
+        vm.error = "You must setup a password for lock your db!"
+      } else if (vm.password.length < 4) {
+        vm.error = "Password must be, at least, 4 chars long!"
+      } else {
+        localStorage.setItem('database', vm.pwdfile.path)
+
+        var low = require('lowdb')
+
+        var db = low(vm.pwdfile.path, {
+          encrypt: true,
+          passkey: vm.password
+        })
+
+        db('settings').push({
+          created: new Date().getTime()
+        })
+
+
+        $window.location.href = '../routers/database.html'
+      }
+    }
+  }
+
+  angular
+    .module('setup')
+    .controller('SetupController', SetupController)
+
+})()
